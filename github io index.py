5ce40d529d487f083fbc4d0c5e30cd7b5cf00daa
Scott Navert <scottnavert@gmail.com>
2024-10-08 21:30:33 -0600

    Create github io index.py

---
+++ github io index.py
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Hi, my name is Scott. Welcome to my website!</h1>
+<p>I'm a Revenue and Product focused analyst, aspiring data scientist, and enthusastic investigator into the things that I find most interesting. Please find my work on Sports, Hospitality & Advertising, and Games here!<p>
+</body>
+</html> 
